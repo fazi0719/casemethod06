519a12e7e9d32152dc929b4a3e92f2e2efd9bc51
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-10-08 20:12:22 +0700

    mata kuliah 2

--- statusKelulusan.java
+++ statusKelulusan.java
@@ -18,7 +18,15 @@ public class statusKelulusan {
         System.out.print("Masukkan nilai Tugas\t: ");
         int tugas1 = sc.nextInt();
         
-            
+        // memasukkan mata kuliah 2
+        System.out.println("\n----- Mata Kuliah 2\t: Struktur Data -----");
+        System.out.print("Masukkan nilai UTS\t: ");
+        int uts2 = sc.nextInt();
+        System.out.print("Masukkan nilai UAS\t: ");
+        int uas2 = sc.nextInt();
+        System.out.print("Masukkan nilai Tugas\t: ");
+        int tugas2 = sc.nextInt();
+
 
         sc.close();
     }
