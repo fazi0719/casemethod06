fdc8cbb5eca0feb401ec9a8cea9fbf029174bca8
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-10-08 19:46:32 +0700

    input nama dan nim

---
+++ statusKelulusan.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class statusKelulusan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("====== INPUT DATA MAHASISWA ======");
+        System.out.print("NAMA\t: ");
+        String nama = sc.nextLine();
+        System.out.print("NIM\t: ");
+        String nim = sc.nextLine();
+        
+            
+
+        sc.close();
+    }
+}
+
